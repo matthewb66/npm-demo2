e669697f3f39ceab80366c1f11da10f475757866
Matthew Brady <w3matt@gmail.com>
2022-01-09 19:21:52 +0000

    Update primes.js

--- primes.js
+++ primes.js
@@ -1,7 +1,7 @@
 exports.isPrime = function(number){
     for(var i = 2; i < number/2; i++) {
         if(number % i === 0) {
-            return false;
+            return true;
         }
     }
     return number > 1;
